36e7844c4411168dad3b5991e7c6029026005e65
Gui Silva <adrianon10@gmail.com>
2025-02-20 04:19:57 -0300

    feat: add helper method show

--- backend/src/app/contracts/controllers/doctors-controller.ts
+++ backend/src/app/contracts/controllers/doctors-controller.ts
@@ -2,4 +2,5 @@ import type { Request, Response } from 'express'
 
 export interface IDoctorsController {
   list: (request: Request, response: Response) => Promise<void>
+  show: (request: Request, response: Response) => Promise<void>
 }
--- backend/src/app/controllers/doctors-controller.ts
+++ backend/src/app/controllers/doctors-controller.ts
@@ -1,9 +1,20 @@
 import type { Request, Response } from 'express'
+
+import { DoctorsRepository } from '@/app/repositories/doctors/doctors-repository'
 import type { IDoctorsController } from '../contracts/controllers/doctors-controller'
-import { DoctorsRepository } from '../repositories/doctors-repository'
+import { GetDoctorByIdUseCase } from '../use-cases/doctor/get-doctor-by-id'
 import { ListDoctorUseCase } from '../use-cases/doctor/list-doctor'
 
 export class DoctorsController implements IDoctorsController {
+  public async show(request: Request, response: Response) {
+    const { id } = request.params
+    const doctorRepository = new DoctorsRepository()
+    const getDoctorByIdUseCase = new GetDoctorByIdUseCase(doctorRepository)
+    const doctor = await getDoctorByIdUseCase.execute(id)
+
+    response.status(200).json(doctor)
+  }
+
   public async list(request: Request, response: Response) {
     const doctorRepository = new DoctorsRepository()
     const doctorUseCase = new ListDoctorUseCase(doctorRepository)
