a326236a80284ace0fd8ccffa95ddffdb0f44c06
Gui Silva <adrianon10@gmail.com>
2025-02-21 04:31:15 -0300

    feat: add create patient use case

---
+++ backend/src/app/use-cases/patient/create-patient.ts
@@ -0,0 +1,39 @@
+import type { IPatientsRepositoryContract } from '@/app/contracts/repositories/patients/patients-repository-contract'
+import type { IUsersRepositoryContract } from '@/app/contracts/repositories/users/users-repository-contract'
+import { hashPassword } from '@/app/helpers/bcrypt-helper'
+
+interface ICreatePatientProps {
+  name: string
+  password: string
+  phone: string
+}
+
+export class CreatePatientUseCase {
+  constructor(
+    private usersRepository: IUsersRepositoryContract,
+    private patientsRepository: IPatientsRepositoryContract,
+  ) {}
+
+  public async execute({ name, password, phone }: ICreatePatientProps) {
+    const patientExists = await this.patientsRepository.getPatientByPhone(phone)
+
+    if (patientExists) {
+      throw new Error('Patient already exists with this phone number')
+    }
+
+    const hashedPassword = await hashPassword(password)
+    const user = await this.usersRepository.create({
+      phone,
+      password: hashedPassword,
+    })
+    const patient = await this.patientsRepository.createPatient({
+      user_id: user.id,
+      name,
+      phone,
+    })
+
+    return {
+      patient_id: patient.id,
+    }
+  }
+}
